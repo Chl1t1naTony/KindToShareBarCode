a19d95ca8ef381ae13df1c59f4cb66c9a44aac26
Chl1t1naTony <127829175+Chl1t1naTony@users.noreply.github.com>
2023-03-14 16:06:46 +0800

    log to data

--- BarCode/Main.js
+++ BarCode/Main.js
@@ -1,11 +1,11 @@
 	let SHEET_ID = '1RY6MBIJv6Vw6r4167p2-Jc7l-izYZAScMb_HONDiyxw'
 	let SHEET_TITLE = 'BarCode'
-	let SHEET_RANGE = 'A1:B2'
+	let SHEET_RANGE = 'A:B'
 		let FULL_URL = ('https://docs.google.com/spreadsheets/d/' + SHEET_ID + '/gviz/tq?sheet=' + SHEET_TITLE + '&range=' + SHEET_RANGE);
 	fetch(FULL_URL)
 	.then(res => res.text())
 	.then(rep => {
     let data = JSON.parse(rep.substr(47).slice(0,-2));
-    console.log(rep)
+    console.log(data)
  
 })
